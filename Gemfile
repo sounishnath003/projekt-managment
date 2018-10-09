b3ec049ff588e9c8f03b4eee2fd60f8cabeea416
sounishnath003 <flock.sinasini@gmail.com>
2018-10-09 18:34:24 +0530

    Succesfully implement Confirmable Email signup system with Rails

--- Gemfile
+++ Gemfile
@@ -29,6 +29,18 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'bulma-rails'
+gem 'simple_form'
+gem 'devise'
+gem 'gravatar_image_tag'
+gem 'jquery-rails'
+gem "wysiwyg-rails"
+gem 'public_activity', '~> 1.5'
+gem "figaro"
+
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ # resources :projects
+ # resources :teams
+
+ 	as :user do
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  end
+
+   devise_for :users, controllers: {
+  	registrations: 'registrations',
+  	confirmation: 'confirmations'
+  }
+  
+  root 'home#index'
 end
